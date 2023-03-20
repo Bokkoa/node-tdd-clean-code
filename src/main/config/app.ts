e1bd86c54487779d6a06b320355c0c70a830f3b1
Bokkoa <krivozznova@gmail.com>
2023-03-19 22:24:38 -0600

    chore: adding middleware settings

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,10 +1,12 @@
-import * as express from 'express'
+import express from 'express'
 import { config as dotenv } from 'dotenv'
 import routes from './routes'
+import middlewares from './middlewares'
 
 dotenv()
 const app = express()
-app.use(express.json())
+// app.use(express.json())
+middlewares(app)
 
 // ROUTES INIT
 routes(app)
